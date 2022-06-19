5feee1c7bbddc69cb3c516103431dab46338a5a2
Chandranshu <chandranshu040@gmail.com>
2022-06-19 15:49:43 +0530

    added signup login routes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+import { loginUser, registerUser } from '../controllers/auth.controller';
 
 const router = express.Router();
+
+router.post("/signup", registerUser);
+
+router.post("/login", loginUser);
 
 export default router;
