907f51647bc90cca3016ff3779a854f1383fe4e6
Chandranshu <chandranshu040@gmail.com>
2022-06-19 13:29:42 +0530

    added more configs

---
+++ src/configs/config.ts
@@ -0,0 +1,5 @@
+export const NODE_ENV = process.env.NODE_ENV;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const ORIGIN = process.env.ORIGIN;
+export const FAST2SMS = process.env.FAST2SMS
+export const ADMIN_PHONE = process.env.ADMIN_PHONE
